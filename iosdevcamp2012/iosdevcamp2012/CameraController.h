b01765dabc941656546075e2bf44d43e0494c3e1
Mark Burger <mburger@millennialmedia.com>
2012-07-21 15:15:33 -0700

    Adding libraries and foundations for OCR and camera functionality.

---
+++ iosdevcamp2012/iosdevcamp2012/CameraController.h
@@ -0,0 +1,13 @@
+//
+//  CameraController.h
+//  iosdevcamp2012
+//
+//  Created by Mark Burger on 7/21/12.
+//  Copyright (c) 2012 Millennial Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraController : UIView
+
+@end
---
+++ iosdevcamp2012/iosdevcamp2012/CameraController.m
@@ -0,0 +1,31 @@
+//
+//  CameraController.m
+//  iosdevcamp2012
+//
+//  Created by Mark Burger on 7/21/12.
+//  Copyright (c) 2012 Millennial Media. All rights reserved.
+//
+
+#import "CameraController.h"
+
+@implementation CameraController
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
